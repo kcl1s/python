1835aa38ed676c75e16ec7cb6222662172029ef2
kcl1s <lohmeyer.keith@gmail.com>
2022-09-13 11:01:59 -0400

    Add files via upload

--- filestuff.py
+++ filestuff.py
@@ -2,15 +2,37 @@ import shutil
 import glob
 import os
 import datetime
+from RPLCD.i2c import CharLCD
 
-stats=shutil.disk_usage('c:')
-print ('Drive space for c: drive in bytes-', stats)
+stats=shutil.disk_usage('/')
+print ('Drive space for root drive in bytes-', stats)
 
-list_of_files = glob.glob('C:/Users/lohme/Pictures/*.*') # * means all if need specific format then *.csv
-print ('Number of files ', len(list_of_files))
-latest_file = max(list_of_files, key=os.path.getctime)
-print('Newest File in Pictures directory ',latest_file)
-epochTime=os.path.getctime(latest_file)
+fileList = glob.glob('/home/pi/Pictures/*.*') # * means all if need specific format then *.csv
+print ('Number of files ', len(fileList))
+latestFile = max(fileList, key=os.path.getctime)
+print('Newest File in Pictures directory ',latestFile)
+epochTime=os.path.getctime(latestFile)
 print ('seconds since 1/1/1970 ?? ',epochTime)
 fileDate=datetime.datetime.fromtimestamp(epochTime)
 print ('latest file created ',fileDate)
+
+freeSpace='Free Space '+str(round((stats.free/10e9),2))+' GB'
+newestFileName=latestFile.rsplit('/',1)[1]
+numFiles='# Files- '+str(len(fileList))
+shortDT=fileDate.strftime('%m/%d/%y %H:%M:%S')
+
+lcd = CharLCD('PCF8574', 0x27)
+lcd.cursor_pos=(0,0)
+lcd.write_string(freeSpace)
+lcd.cursor_pos=(1,0)
+lcd.write_string(newestFileName)
+lcd.cursor_pos=(2,0)
+lcd.write_string(numFiles)
+lcd.cursor_pos=(3,0)
+lcd.write_string(shortDT)
+
+# https://docs.python.org/3/library/shutil.html#shutil.disk_usage 
+# https://docs.python.org/3/library/glob.html
+# https://docs.python.org/3/library/os.path.html
+# https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
+# https://rplcd.readthedocs.io/en/stable/index.html
